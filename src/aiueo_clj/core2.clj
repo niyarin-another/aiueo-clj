d500dd154bd625a6b608949ba4deca31401fe600
niyarin <inoanet@gmail.com>
2020-05-29 16:18:29 +0900

    f commit

---
+++ src/aiueo_clj/core2.clj
@@ -0,0 +1,20 @@
+(ns aiueo-clj.core2)
+
+
+(defn bar2 
+[x y z]
+"BAD COMMENT"
+(+ x 1))
+
+(defn- boooooo [x q]
+    "piyo"
+    (if  (= 0 0)
+        (and x)))
+
+(defn foo2
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
+
+
+'HELLO2
---
+++ src/aiueo_clj/core3.clj
@@ -0,0 +1,20 @@
+(ns aiueo-clj.core3)
+
+(defn bar 
+[x y z]
+"BAD COMMENT"
+(+ x 1))
+
+
+(defn- boooooo [x q]
+    "piyo"
+    (if  (= 0 0)
+        (and x)))
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
+
+
+'HELLO
