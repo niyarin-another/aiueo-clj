eb4831e3986180ab95f1c81e56eb855aa7207523
niyarin <inoanet@gmail.com>
2020-06-01 11:52:20 +0900

    HELLO3

---
+++ src/aiueo_clj/core5.clj
@@ -0,0 +1,26 @@
+(ns aiueo-clj.core)
+
+(defn bar 
+[x y z]
+"BAD COMMENT"
+(+ x 1))
+(defn- boooooo [x q]
+    "piyo"
+    (if  (= 0 0)
+        (and x)))
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
+
+
+
+'HELLO
+
+;a
+
+
+
+
+
