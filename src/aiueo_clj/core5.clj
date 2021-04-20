0c657a9b2d7bf6e046e524104ed2c14ac0fa2bb6
niyarin-another <inoue@csg.ci.i.u-tokyo.ac.jp>
2021-04-20 19:31:20 +0900

    update

--- src/aiueo_clj/core5.clj
+++ src/aiueo_clj/core5.clj
@@ -1,13 +1,6 @@
 (ns aiueo-clj.core)
 
-(defn bar 
-[x y z]
-"BAD COMMENT"
-(+ x 1))
-(defn- boooooo [x q]
-    "piyo"
-    (if  (= 0 0)
-        (and x)))
+(defn bar [x] (inc x))
 
 (defn foo
   "I don't do a whole lot."
